161952d1026a41dd8f6dfa8981810f7d09b60b11
Kavyaeprakash <122441657+Kavyaeprakash@users.noreply.github.com>
2025-07-04 13:49:20 +0530

    Update settings.py

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'hospital_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME':'hospitaldb',
-        'USER':'postgres',
-        'PASSWORD':'root',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
